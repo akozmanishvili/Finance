ca88b00834211ba6f8897381136d3e31e15d364c
AMIKO <akozmanishvili@gmail.com>
2026-06-30 19:32:13 +0400

    commit: created router for pages in main.jsx, imported outlet in App.jsx for router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>App</div>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Categories from "./pages/Categories.jsx";
+import Stats from "./pages/Stats.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <Error></Error>,
+    children: [
+      { index: true, element: <Home></Home> },
+      { path: "/login", element: <Login></Login> },
+      { path: "/categories", element: <Categories></Categories> },
+      { path: "/stats", element: <Stats></Stats> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
