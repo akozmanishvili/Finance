54ac10532c91238faaf5b96072e8db92a645a8ef
AMIKO <akozmanishvili@gmail.com>
2026-07-05 15:20:54 +0400

    commit: modified Navbar.jsx: added logout button to it which redirect user to login page. Also added feature: if user is logged in, login page link is hidden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
-
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const { logout, loggedIn } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    logout();
+    return navigate("/login");
+  };
   return (
     <div>
       <Link to={"/"}>Home Page</Link>
-      <Link to={"/login"}>Login</Link>
+      {loggedIn || <Link to={"/login"}>Login</Link>}
       <Link to={"/categories"}>Categories</Link>
       <Link to={"/stats"}>Statistics</Link>
+      <button type="button" onClick={handleSubmit}>
+        Log out
+      </button>
     </div>
   );
 };
