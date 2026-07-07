d80541dc70a7b6bbfdece6a19186605472cfd2c4
AMIKO <akozmanishvili@gmail.com>
2026-07-07 10:28:17 +0400

    commit: fixed the bag in Stats.jsx. Moved all working code into StatsInfo.jsx component. Imported component to Stats.jsx page

--- src/components/StatsInfo.jsx
+++ src/components/StatsInfo.jsx
@@ -1,5 +1,86 @@
+import useData from "../hooks/useData";
+
 const StatsInfo = () => {
-  return <div>StatsInfo</div>;
+  const { transactions, categories } = useData();
+
+  const totalSpending = Number(
+    transactions
+      .filter((transaction) => transaction.type === "Expense")
+      .reduce((sum, n) => {
+        sum = sum + Number(n.cost);
+        return sum;
+      }, 0),
+  );
+
+  const totalIncome = Number(
+    transactions
+      .filter((transaction) => transaction.type === "Income")
+      .reduce((sum, n) => {
+        sum = sum + Number(n.cost);
+        return sum;
+      }, 0),
+  );
+
+  const netBalance = totalIncome - totalSpending;
+  let savingsRate = 0;
+
+  if (netBalance > 0) {
+    savingsRate = `${(100 * netBalance) / totalIncome}%`;
+  } else {
+    savingsRate = "Spent more than Income";
+  }
+
+  return (
+    <div>
+      <h2>Total Spending: {totalSpending}</h2>
+      <h2>Total Income: {totalIncome}</h2>
+      <h3>Net Balance: {netBalance}</h3>
+      <h3>Savings rate: {savingsRate}</h3>
+      <div>
+        {categories.map((categoryParam) => {
+          const eachCategoryExpenses = transactions.filter(
+            (transaction) =>
+              transaction.category === categoryParam.name &&
+              transaction.type === "Expense",
+          );
+          const eachCategorySpending = eachCategoryExpenses.reduce((sum, n) => {
+            sum = sum + Number(n.cost);
+            return sum;
+          }, 0);
+          if (eachCategorySpending === 0) return;
+
+          return (
+            <h3 key={categoryParam.id}>
+              {categoryParam.name} spent{" "}
+              {(Number(eachCategorySpending) * 100) / totalSpending}% of total
+              expenses
+            </h3>
+          );
+        })}
+      </div>
+      <div>
+        {categories.map((categoryParam) => {
+          const eachCategoryIncomes = transactions.filter(
+            (transaction) =>
+              transaction.category === categoryParam.name &&
+              transaction.type === "Income",
+          );
+          const eachCategorySpending = eachCategoryIncomes.reduce((sum, n) => {
+            sum = sum + Number(n.cost);
+            return sum;
+          }, 0);
+          if (eachCategorySpending === 0) return;
+          return (
+            <h3 key={categoryParam.id}>
+              {categoryParam.name} is{" "}
+              {(Number(eachCategorySpending) * 100) / totalIncome}% of total
+              incomes
+            </h3>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default StatsInfo;
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,60 +1,9 @@
-import useData from "../hooks/useData";
+import StatsInfo from "../components/StatsInfo";
+
 const Stats = () => {
-  const { transactions, categories } = useData();
-
-  const totalSpending = Number(
-    transactions
-      .filter((transaction) => transaction.type === "Expense")
-      .reduce((sum, n) => {
-        sum = sum + n.cost;
-        return sum;
-      }, 0),
-  );
-
-  const totalIncome = Number(
-    transactions
-      .filter((transaction) => transaction.type === "Income")
-      .reduce((sum, n) => {
-        sum = sum + n.cost;
-        return sum;
-      }, 0),
-  );
-
-  const netBalance = totalIncome - totalSpending;
-  let savingsRate = 0;
-
-  if (netBalance > 0) {
-    savingsRate = (100 * netBalance) / totalIncome;
-  } else {
-    savingsRate = "Spent more than Income";
-  }
-
   return (
     <div>
-      <h2>Total Spending: {totalSpending}</h2>
-      <h2>Total Income: {totalIncome}</h2>
-      <h3>Net Balance: {netBalance}</h3>
-      <h3>Savings rate: {savingsRate}%</h3>
-      <div>
-        {categories.map((categoryParam) => {
-          const eachCategoryTransaction = transactions.filter(
-            (transaction) => transaction.category === categoryParam.category,
-          );
-          const eachCategorySpending = eachCategoryTransaction.reduce(
-            (sum, n) => {
-              sum = sum + n;
-              return sum;
-            },
-            0,
-          );
-          return (
-            <h3 key={categoryParam.id}>
-              {categoryParam.category} spent{" "}
-              {(eachCategorySpending * 100) / totalSpending} of total expenses
-            </h3>
-          );
-        })}
-      </div>
+      <StatsInfo></StatsInfo>
     </div>
   );
 };
